da36872067b6a360b3069e2d09b0bd760a45f94b
Yahia-Ka <yahia.karkori@um5r.ac.ma>
2023-04-03 03:56:02 +0000

    1st commit just joking around

--- app.js
+++ app.js
@@ -1,198 +1,303 @@
-import avCarList from "./cars_data.js"
+// Animation Timeline
+const animationTimeline = () => {
+  // Spit chars that needs to be animated individually
+  const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0]
+  const hbd = document.getElementsByClassName("wish-hbd")[0]
 
-const mockApiUrl = "https://63f0dabaff1b45a1a442cf7e.mockapi.io/api/Cars"
+  textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
+    .split("")
+    .join("</span><span>")}</span`
 
-// Getting dom elms needed to work with
-const upcomingSec = document.querySelector(".notav-section")
-const availableSec = document.querySelector(".available-section")
-const singElemSec = document.querySelector(".single-elem")
-const basedOnCategory = document.querySelectorAll(".ctgory-btn")
-const rentBtns = document.querySelectorAll(".rent-btn")
+  hbd.innerHTML = `<span>${hbd.innerHTML.split("").join("</span><span>")}</span`
 
-//tried to get data from a mock api and display using fetch
-const showFeatured = async () => {
-  try {
-    const res = await fetch(mockApiUrl)
-    // return res.json()
-    const data = await res.json()
-    upcomingSec.innerHTML = data
-      .map(
-        (car) =>
-          `<article class="cars-item">
-              <img src="./Car_imges/unrevealed_car.png" alt="cars item" class="no-photo" />
-              <div class="item-info">
-                <header>
-                  <h4>${car.title}</h4>
-                  <h4 class="price"> $${car.price}/day</h4>
-                </header>
-                <p class="item-text">
-                ${car.description}
-                </p>
-              </div>
-            </article>`
-      )
-      .join("")
-  } catch (err) {
-    console.log(err)
+  const ideaTextTrans = {
+    opacity: 0,
+    y: -20,
+    rotationX: 5,
+    skewX: "15deg",
   }
-}
-// same as above but now with local data
-const showAvailable = () => {
-  availableSec.innerHTML = avCarList
-    .map(
-      (car) => `<article id=${car.id} class="cars-item">
-              <img src="${car.img}" alt="cars item" class="photo" />
-              <div class="item-info">
-                <header>
-                  <h4>${car.title}</h4>
-                  <h4 class="price">$${car.price}/day</h4>
-                </header>
-                <p class="item-text">
-                ${car.desc}
-                <div class="item-btns">
-                <a href="#selectedItem"><button type="button"  class="rent-btn" id="${car.id}">Rent</button></a>
-                </div>
-                </p>
-              </div>
-              
-            </article>`
-    )
-    .join(" ")
-}
-// dispalayed as soon as the e fires
-window.addEventListener("DOMContentLoaded", showFeatured(), showAvailable())
 
-// handling inputs on search input
+  const ideaTextTransLeave = {
+    opacity: 0,
+    y: 20,
+    rotationY: 5,
+    skewX: "-15deg",
+  }
 
-const srchInput = document.querySelector(".srchinput")
-srchInput.addEventListener("change", (e) => {
-  let currVal = e.target.value
-  // console.log(currVal)
+  const tl = new TimelineMax()
 
-  const isFound = new Set()
-
-  for (let item of avCarList)
-    if (
-      Reflect.getOwnPropertyDescriptor(item, "title").value ===
-      currVal.toLowerCase()
-    )
-      isFound.add(currVal)
-
-  isFound.has(currVal)
-    ? alert(`${currVal} deos exist`)
-    : alert(`we dont have ${currVal} right now`)
-})
-
-// Tried to filter fetched data based on category  button clicks
-basedOnCategory.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const fuel = e.currentTarget.innerHTML.trim()
-    // console.log(fuel)
-    fetch(mockApiUrl)
-      .then((res) => res.json())
-      .then(
-        (data) =>
-          (upcomingSec.innerHTML = data
-            .filter((car) => car.category === fuel)
-            .map(
-              (item) =>
-                `<article class="cars-item">
-                  <img src="./Car_imges/unrevealed_car.png" alt="cars item" class="photo" />
-                  <div class="item-info">
-                    <header>
-                      <h4>${item.title}</h4>
-                      <h4 class="price"> $${item.price}/day</h4>
-                    </header>
-                    <p class="item-text">
-                    ${item.description}
-                    </p>
-                  </div>
-                </article>`
-            )
-            .join(""))
-      )
-      .catch((error) => console.log(error))
-    if (fuel === "ō͡≡o") {
-      // console.log("something went wrong!")
-      showFeatured()
-    }
+  tl.to(".container", 0.1, {
+    visibility: "visible",
   })
-})
+    .from(".one", 0.7, {
+      opacity: 0,
+      y: 10,
+    })
+    .from(".two", 0.4, {
+      opacity: 0,
+      y: 10,
+    })
+    .to(
+      ".one",
+      0.7,
+      {
+        opacity: 0,
+        y: 10,
+      },
+      "+=2.5"
+    )
+    .to(
+      ".two",
+      0.7,
+      {
+        opacity: 0,
+        y: 10,
+      },
+      "-=1"
+    )
+    .from(".three", 0.7, {
+      opacity: 0,
+      y: 10,
+      // scale: 0.7
+    })
+    .to(
+      ".three",
+      0.7,
+      {
+        opacity: 0,
+        y: 10,
+      },
+      "+=2"
+    )
+    .from(".four", 0.7, {
+      scale: 0.2,
+      opacity: 0,
+    })
+    .from(".fake-btn", 0.3, {
+      scale: 0.2,
+      opacity: 0,
+    })
+    .staggerTo(
+      ".hbd-chatbox span",
+      0.5,
+      {
+        visibility: "visible",
+      },
+      0.05
+    )
+    .to(".fake-btn", 0.1, {
+      backgroundColor: "rgb(127, 206, 248)",
+    })
+    .to(
+      ".four",
+      0.5,
+      {
+        scale: 0.2,
+        opacity: 0,
+        y: -150,
+      },
+      "+=0.7"
+    )
+    .from(".idea-1", 0.7, ideaTextTrans)
+    .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-2", 0.7, ideaTextTrans)
+    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-3", 0.7, ideaTextTrans)
+    .to(".idea-3 strong", 0.5, {
+      scale: 1.2,
+      x: 10,
+      backgroundColor: "rgb(21, 161, 237)",
+      color: "#fff",
+    })
+    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-4", 0.7, ideaTextTrans)
+    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(
+      ".idea-5",
+      0.7,
+      {
+        rotationX: 15,
+        rotationZ: -10,
+        skewY: "-5deg",
+        y: 50,
+        z: 10,
+        opacity: 0,
+      },
+      "+=0.5"
+    )
+    .to(
+      ".idea-5 span",
+      0.7,
+      {
+        rotation: 90,
+        x: 8,
+      },
+      "+=0.4"
+    )
+    .to(
+      ".idea-5",
+      0.7,
+      {
+        scale: 0.2,
+        opacity: 0,
+      },
+      "+=2"
+    )
+    .staggerFrom(
+      ".idea-6 span",
+      0.8,
+      {
+        scale: 3,
+        opacity: 0,
+        rotation: 15,
+        ease: Expo.easeOut,
+      },
+      0.2
+    )
+    .staggerTo(
+      ".idea-6 span",
+      0.8,
+      {
+        scale: 3,
+        opacity: 0,
+        rotation: -15,
+        ease: Expo.easeOut,
+      },
+      0.2,
+      "+=1"
+    )
+    .staggerFromTo(
+      ".baloons img",
+      2.5,
+      {
+        opacity: 0.9,
+        y: 1400,
+      },
+      {
+        opacity: 1,
+        y: -1000,
+      },
+      0.2
+    )
+    .from(
+      ".lydia-dp",
+      0.5,
+      {
+        scale: 3.5,
+        opacity: 0,
+        x: 25,
+        y: -25,
+        rotationZ: -45,
+      },
+      "-=2"
+    )
+    .from(".hat", 0.5, {
+      x: -100,
+      y: 350,
+      rotation: -180,
+      opacity: 0,
+    })
+    .staggerFrom(
+      ".wish-hbd span",
+      0.7,
+      {
+        opacity: 0,
+        y: -50,
+        // scale: 0.3,
+        rotation: 150,
+        skewX: "30deg",
+        ease: Elastic.easeOut.config(1, 0.5),
+      },
+      0.1
+    )
+    .staggerFromTo(
+      ".wish-hbd span",
+      0.7,
+      {
+        scale: 1.4,
+        rotationY: 150,
+      },
+      {
+        scale: 1,
+        rotationY: 0,
+        color: "#ff69b4",
+        ease: Expo.easeOut,
+      },
+      0.1,
+      "party"
+    )
+    .from(
+      ".wish h5",
+      0.5,
+      {
+        opacity: 0,
+        y: 10,
+        skewX: "-15deg",
+      },
+      "party"
+    )
+    .staggerTo(
+      ".eight svg",
+      1.5,
+      {
+        visibility: "visible",
+        opacity: 0,
+        scale: 80,
+        repeat: 3,
+        repeatDelay: 1.4,
+      },
+      0.3
+    )
+    .to(".six", 0.5, {
+      opacity: 0,
+      y: 30,
+      zIndex: "-1",
+    })
+    .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
+    .to(
+      ".last-smile",
+      0.5,
+      {
+        rotation: 90,
+      },
+      "+=1"
+    )
 
-// getting form entries => posting to the api \\
+  // tl.seek("currentStep");
+  // tl.timeScale(2);
 
-const form = document.getElementById("feedback")
-form.addEventListener("submit", (e) => {
-  e.preventDefault()
-  const clientInfo = new FormData(form)
-  const _jsonFormat = JSON.stringify(Object.fromEntries(clientInfo))
-
-  fetch("https://63f0dabaff1b45a1a442cf7e.mockapi.io/api/ClientsFeedback", {
-    method: "POST",
-    body: _jsonFormat,
-    headers: { "content-type": "application/json" },
+  // Restart Animation on click
+  const replyBtn = document.getElementById("replay")
+  replyBtn.addEventListener("click", () => {
+    tl.restart()
   })
-    .then((resp) => resp.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error))
-})
+}
 
-// const toggleItem = () => {
-//   const disp = singElemSec.style.display
-//   disp === "none" ? (disp = "block") : (disp = "none")
-// }
-
-// const endOfToggle = () => {
-// }
-let btns = document.querySelectorAll(".rent-btn")
-
-for (let b of btns) {
-  ;((b) => {
-    b.addEventListener("click", function (e) {
-      // console.log(typeof e.currentTarget.id)
-      avCarList.forEach((obj) => {
-        if (obj.id == e.currentTarget.id) {
-          console.log("executed1")
-          // singElemSec.tog
-          singElemSec.innerHTML = avCarList
-            .filter((item) => item.id == e.currentTarget.id) // double instead of triple = sign, get rid of types constraint
-            .map(
-              () => `<article id=${obj.id} class="selected-car" id="#selectedItem">
-                    <h3> This should be on another page</h3>
-                 <img src="${obj.img}" alt="cars item" class="photo" />
-                 <div class="item-info">
-                   <header>
-                     <h4>${obj.title}</h4>
-                     <h4 class="price"> ${obj.price} MAD/jour</h4>
-                   </header>
-                   <p class="item-text">
-                     ${obj.desc}
-                   </p>
-                  
-                 </div>
-                 <h2>Specify car pick-up date </p>
-                 <input type="date" id="date">
-               </article>`
-            )
+// Import the data to customize and insert them into page
+const fetchData = () => {
+  fetch("customize.json")
+    .then((data) => data.json())
+    .then((data) => {
+      Object.keys(data).map((customData) => {
+        if (data[customData] !== "") {
+          if (customData === "imagePath") {
+            document
+              .getElementById(customData)
+              .setAttribute("src", data[customData])
+          } else {
+            document.getElementById(customData).innerText = data[customData]
+          }
         }
       })
     })
-  })(b)
 }
 
-// presentational tweaks, scrolls on clicks....
-const topFnc = () => (document.documentElement.scrollTop = 0)
+// Run fetch and animation in sequence
+const resolveFetch = () => {
+  return new Promise((resolve, reject) => {
+    fetchData()
+    resolve("Fetch done!")
+  })
+}
 
-let topBtn = document.getElementById("toTopBtn")
-topBtn.addEventListener("click", (e) => {
-  topFnc()
-})
-// 30px scrolls down, toTp btn appears
-window.onscroll = () => scrollFnc()
-
-const scrollFnc = () =>
-  document.body.scrollTop > 30 || document.documentElement.scrollTop > 30
-    ? (topBtn.style.display = "block")
-    : (topBtn.style.display = "none")
-
-// using Proxy
+resolveFetch().then(animationTimeline())
